985f8e45b111b40f677a1dc98a0d785f68ebf7fe
Simone Berbert Rodrigues Dapólito <simone.berbert@gmail.com>
2026-02-08 20:37:39 -0300

    first commit projeto 1

---
+++ projeto1/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Saudacao from './components/Saudacao'
+import Nome from './components/Nome'
+import Curso from './components/Curso'
+
+function App() {
+
+  return (
+    <>
+      <h1>Minha 1a Aplicação React</h1>
+      <Saudacao />   
+      <Nome />
+      <Curso />   
+    </>    
+  )
+}
+
+export default App  
---
+++ projeto1/src/components/Nome.jsx
@@ -0,0 +1,11 @@
+function Nome() {
+  const nome = "Simone"
+  return (
+    <div>  
+        <p><b>Nome do aluno:</b> {nome}</p>
+        <hr />
+    </div>
+  )
+}
+
+export default Nome
